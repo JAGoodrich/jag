d3901e955e8af34fd494b037a69d779ab947c440
JAGoodrich <32161948+JAGoodrich@users.noreply.github.com>
2018-01-10 16:08:58 -0700

    January 2018

--- R/reliability.l.R
+++ R/reliability.l.R
@@ -1,15 +1,20 @@
 #' @export
 #' @importFrom irr icc
 #' @import dplyr
-#'
+#' 
 reliability.l <- function(fx, id, data, iv_name = NULL){
   data <- droplevels(data)
-  fx.terms <- terms(fx)
+  fx.terms <- terms(fx) 
   response <- all.vars(fx)[attributes(fx.terms)$response]
   iv.names <- attributes(terms(fx))$term.labels[which(attributes(fx.terms)$order == 1)]
-  if(length(iv.names) > 1) {return("More than one independent variable not supported")}
-  d <- data.frame(dv = data[[response]], id= as.factor(data[[id]]), iv = as.factor(data[[iv.names]]))
-  if(length(levels(d$iv)) != 2) {return("indepenent variable must contain exactly two levels")}
+  if(!response[1] %in% colnames(data)) {stop(paste("variable ",  response, "not found in data frame"))}
+  if(length(iv.names) > 1) {stop("More than one independent variable not supported")}
+  if(!iv.names[1] %in% colnames(data)) {stop(paste("variable ",  iv.names, "not found in data frame"))}
+  if(!id %in% colnames(data)) {stop(paste("variable ",  id, "not found in data frame"))}
+  d <- data.frame(dv = data[[response]], 
+                  id = as.factor(data[[id]]), 
+                  iv = as.factor(data[[iv.names]]))
+  if(length(levels(d$iv)) != 2) {stop("Indepenent variable must contain exactly two levels")}
   l <- levels(d$iv)
   temp1 <- d[ d$iv == l[1], 1:2]
   temp2 <- d[ d$iv == l[2], 1:2]
@@ -29,23 +34,23 @@ reliability.l <- function(fx, id, data, iv_name = NULL){
   CD <- mean(d$delta) / sd(d$delta)
   CV <- 100*(exp((sd(d$lndelta)/sqrt(2))/100)-1)
   ICC <- irr::icc(d[,c(1,2)], model = "twoway", type = "agreement")$value
-  #par(mfrow=c(1,3))
-  plot(d$var_1, d$var_2, xlim=range(d$var_1, d$var_2), ylim=range(d$var_1, d$var_2), xlab = l[1], ylab = l[2], main = iv_name)
+  par(mfrow=c(1,3))
+  plot(d$var_1, d$var_2, xlim=range(d$var_1, d$var_2), ylim=range(d$var_1, d$var_2), xlab = l[1], ylab = l[2])
   abline(0, 1, col = "red", lty = 2); abline(lm(d$var_2 ~ d$var_1), lty = 1)  #Residuals vs. fitted of lm(var1, var2)
   boxplot(d[,1:2], names = c(l[1], l[2]),  main = iv_name)
   stripchart(d[,1:2],vertical = TRUE, add = TRUE, method = "jitter")
-  hist(d$pct_chg, xlab="Percent Change (%)", xlim=c(-max(d$pct_chg)-15, max(d$pct_chg)+15), main = NULL, freq = FALSE, main = iv_name)
+  hist(d$pct_chg, xlab="Percent Change (%)", xlim=c(min(d$pct_chg)-15, max(d$pct_chg)+15), main = NULL, freq = FALSE)
   lines(density(d$pct_chg, adjust = 1.2), col = "red")
-  #par(mfrow=c(1,1))
+  par(mfrow=c(1,1))
   cat("\nCall:")
   cat("\n     Variable one: ", l[1], "\n     Variable two: ", l[2], "\n")
   cat("\nDifference in means:")
   print(t.test(d$var_1, d$var_2, paired = TRUE))
-  cat("\nCohens d:", prettyNum(CD))
+  cat("Cohens d:", prettyNum(CD), "\n")
   cat("\nReliability:", "\n")
   cat("\n      Typical error: ", prettyNum(TE))
   cat("\n      Coefficient of Variability: ", prettyNum(CV))
   cat("\n      IntRAclass correlation coefficient: ", prettyNum(ICC))
   cat("\n      Pearson correlation coefficient (r, IntERclass correlation): ", prettyNum(cor(d$var_1, d$var_2)))
-  cat("\n      Coefficient of Variability (r^2): ", prettyNum(cor(d$var_1, d$var_2)^2))
+  cat("\n      Coefficient of Variability (r^2): ", prettyNum(cor(d$var_1, d$var_2)^2), "\n")
 }
